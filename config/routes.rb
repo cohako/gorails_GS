f90c89befef1e6b4d3a3710cc24fc3fdb6ea62b5
cohako <muri.ilo@hotmail.com>
2021-03-05 22:45:50 -0300

    Adicionado forma de trocar senha

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "main#index"
 
   get "about-us", to: "about#index", as: :about
 
-  get "sign_up", to: "registrations#new"
+  get "sign_in", to: "sessions#new"
+  post "sign_in", to: "sessions#create"
 
-  root to: "main#index"
+  get "sign_up", to: "registrations#new"
   post "sign_up", to: "registrations#create"
+
+  get "password", to: "passwords#edit", as: :edit_passwords
+  patch "password", to: "passwords#update"
+  
+  delete "logout", to: "sessions#destroy"
+
 end
